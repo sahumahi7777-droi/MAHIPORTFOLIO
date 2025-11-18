7d94509dddd08929af41541534683c652eb2cbd4
Mahi Sahu <sahumahi7777@gmail.com>
2025-11-18 10:56:24 +0530

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client"),
     },
   },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
 });
 
 
