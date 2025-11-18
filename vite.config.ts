39a3461b272b7be9946e1fc245d203862c2ea6c3
Mahi Sahu <sahumahi7777@gmail.com>
2025-11-18 11:05:20 +0530

    Fix structure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client"),
+      "@": "/src",
     },
   },
   build: {
     outDir: "dist",
-    emptyOutDir: true
-  }
+  },
 });
+
 
 
 
